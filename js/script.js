4e60c4a22e26f2e571c708807e2c4a4a1524bfd9
WintrCat <wintrcat.wilson@gmail.com>
2025-02-05 16:48:12 +0000

    Add check for URL hash for nav styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 // CONSIDER IMPLEMENTING CHECK FOR HASH IN URL AND CHANGE COLOUR OF NAV OPTION
 var links = document.getElementById("nav");
 
+function updateNavFromHash() {
+    const navOptions = links.getElementsByTagName("a");
+
+    for (const navOption of navOptions) {
+        if (navOption.href.endsWith(location.hash)) {
+            // change styles to anything you want for selected nav option
+            navOption.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        } else {
+            navOption.style.backgroundColor = "unset";
+        }
+    }
+}
+
+updateNavFromHash();
+
 /* BURGER MENU */
 // SHOW burger menu
 function showBurger() {
@@ -13,6 +28,8 @@ function showBurger() {
 
 // HIDE burger menu ON WIDE SCREENS 
 function closeBurger() {
+    setTimeout(updateNavFromHash, 100);
+
     if (screen.width <= 1000) {
         links.style.display = "none";
     }
